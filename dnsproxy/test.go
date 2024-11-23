0c868d4739c266045c94abed846e6a1c533ae88e
Your Name <your.email@example.com>
2024-11-23 11:54:21 +0800

    重构了代码

--- dnsproxy/test.go
+++ dnsproxy/test.go
@@ -11,5 +11,5 @@ var password = "12982397StrongPassw0rd"
 func main() {
 	c := omiproxy.NewClient(&redis.Options{Addr: redisAddr, Password: password})
 	proxy := c.NewProxy("http8000", "118.25.196.166:8000", omiproxy.DomainMode)
-	proxy.Start(omiproxy.Https, 1, "certs/stormili.crt", "certs/stormili.key")
+	proxy.Start(omiproxy.Https, 1, "../../certs/stormili.crt", "../../certs/stormili.key")
 }
